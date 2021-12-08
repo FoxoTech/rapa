32c10743c2f45481de7cfce75449630c62e230f1
viabard <joshuaschaaf@gmail.com>
2021-12-08 13:36:17 -0500

    create setup and fix import

--- setup.py
+++ setup.py
@@ -1,21 +1,45 @@
 from setuptools import find_packages, setup
-import re
+exec(open('rapa/_version.py').read())
 
-def get_version():
-    return(re.findall(r"__version__.+\"(.+)\"",open("rapa/_version.py", 'r').read())[0])
 
-rapa_version = get_version()
-print(rapa_version)
+
 
 setup(
     name='rapa',
     packages=find_packages(),
-    version=rapa_version,
+    version=__version__,
     description='Robust Automtated Parsimony Analysis',
-    author='RO, JS',
+    long_description=open('README.md').read(),
+    author='Life Epigenetics',
+    author_email='info@FOXOBioScience.com',
     license='MIT',
-    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Framework :: Jupyter',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+      ],
+    keywords='feature reduction features datarobot robust automated parsimony analysis',
+    install_requires=[
+        'numpy',
+        'pandas >=1.3.0',
+        'scikit-learn',
+        'matplotlib',
+        'datarobot',
+        'tqdm',
+        'pickle',
+        'seaborn'
+    ],
     setup_requires=['pytest-runner'],
-    test_require=['pytest'],
+    tests_require=['pytest'],
     test_suite='tests',
+    url='https://github.com/FoxoTech/rapa',
 )
