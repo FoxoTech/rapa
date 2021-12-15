d7aa95759cfbca17ac8e3afe634537cd56514868
viabard <joshuaschaaf@gmail.com>
2021-12-15 16:38:31 -0500

    autodocs

--- conf.py
+++ conf.py
@@ -12,11 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 print(os.listdir(), os.getcwd())
 
-apidoc_module_dir = '..'
+apidoc_module_dir = '.'
 apidoc_output_dir = '_sphinx_build'
 apidoc_separate_modules = False
 
