d04e5dfd283fa85cc4236aee625e71945866a7f3
viabard <joshuaschaaf@gmail.com>
2021-12-15 16:35:21 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+print(os.listdir(), os.getcwd())
+
 apidoc_module_dir = '..'
 apidoc_output_dir = '_sphinx_build'
 apidoc_separate_modules = False
