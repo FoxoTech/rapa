1a108a1bccaf0352b08dcb7100dd3141fc9c34f7
viabard <joshuaschaaf@gmail.com>
2022-06-28 11:49:23 -0400

    pytest-order & find_project() test
    Added pytest-order to test requirements and added a test for rapa.utils.find_project()

--- tests/utils/test_datarobot_api.py
+++ tests/utils/test_datarobot_api.py
@@ -4,8 +4,11 @@ import rapa
 import os
 import pickle
 
+import datarobot as dr
+
 
 # test api initialization
+@pytest.mark.order(1)
 def test_api_initialization():
     '''Checks that the api can be connected with `utils.initialize_dr_api`.
 
@@ -19,6 +22,7 @@ def test_api_initialization():
     ## create the pickle file
     pickle.dump({'test':dr_test_api_key}, open(pkl_file_name, 'wb'))
 
+    ## try connecting
     try:
         retval = rapa.utils.initialize_dr_api('test', pkl_file_name)
     except ValueError:
@@ -30,3 +34,21 @@ def test_api_initialization():
     os.remove(pkl_file_name) 
 
     assert retval == None
+
+# test retrieval of projects
+@pytest.mark.order(2)
+def test_datarobot_project_retrieval():
+    '''Checks that `rapa` retrieves the correct project from DataRobot.
+    
+    Tests different situations:
+        1. Name provided matches project name exactly, and only one project exists with that name.
+        2. Project ID is provided, and there exists a project with that ID.
+        3. Name provided fetches one project, but not exact.
+        4. Name provided fetches more than one project.
+        5. Name provided fetches no project.
+    '''
+
+    # 1. Name provided matches project name exactly, and only one project exists with that name
+    bc_project = rapa.utils.find_project('TUTORIAL_breast_cancer')
+    assert bc_project is not None, 'Project using exact name not found (check that `TUTORIAL_breast_cancer` still exists)'
+    assert type(bc_project) is dr.Project
