f8744a16dbfe08cbd368547bf0a4287096bd8949
viabard <joshuaschaaf@gmail.com>
2022-08-19 11:52:04 -0400

    added regression submittable dataframe test
    Added a regression object to the submittable dataframe creation test.

--- tests/Project/test_Project_initialization.py
+++ tests/Project/test_Project_initialization.py
@@ -17,9 +17,19 @@ top down (using pytest-order)
 
 created_project_name = f"GA_TESTS rapa=={rapa.version.__version__}"
 
+n_splits = 6
+n_features = 20
+target = "benign"
+
+# loads the dataset (as a dictionary)
+breast_cancer_dataset = datasets.load_breast_cancer()
+# puts features and targets from the dataset into a dataframe
+breast_cancer_df = pd.DataFrame(
+    data=breast_cancer_dataset['data'], columns=breast_cancer_dataset['feature_names'])
+breast_cancer_df[target] = breast_cancer_dataset['target']
+
+
 # initialize dr api
-
-
 @pytest.mark.order(1)
 def test_datarobot_api_initialization():
     """Tests that the datarobot api can be initialized with the test api key.
@@ -45,8 +55,6 @@ def test_Project_initialization():
     assert rapa_test_regression_project._regression == True and rapa_test_regression_project._classification == False
 
 # test creating a submittable dataframe
-
-
 @pytest.mark.order(3)
 def test_creating_submittable_dataframe():
     """Checks that rapa correctly makes a submittable dataframe.
@@ -55,17 +63,10 @@ def test_creating_submittable_dataframe():
         2. Submittable df with feature reduction
         3. Checks submittable df cannot be created with a target not in the original df
     """
-    target = "benign"
-
-    # loads the dataset (as a dictionary)
-    breast_cancer_dataset = datasets.load_breast_cancer()
-    # puts features and targets from the dataset into a dataframe
-    breast_cancer_df = pd.DataFrame(
-        data=breast_cancer_dataset['data'], columns=breast_cancer_dataset['feature_names'])
-    breast_cancer_df[target] = breast_cancer_dataset['target']
 
     bc_classification = rapa.Project.Classification()  # rapa classification project
-    n_splits = 6
+    bc_regression = rapa.Project.Regression()  # rapa classification project
+    
 
     # 1. Submittable dataframe with no initial feature reduction
     sub_df = bc_classification.create_submittable_dataframe(breast_cancer_df,
@@ -78,10 +79,21 @@ def test_creating_submittable_dataframe():
     # check the number of splits
     assert len(sub_df["partition"].value_counts()) == n_splits
 
-    n_features = 20
+    
 
-    # 2. Submittalbe dataframe with initial feature reduction
+    # 2. Submittable dataframe with initial feature reduction
+    # classification
     sub_df = bc_classification.create_submittable_dataframe(breast_cancer_df,
+                                                            target_name=target,
+                                                            n_splits=6,
+                                                            random_state=conf.random_state,
+                                                            n_features=n_features)
+    # checking that the correct number of featres exists
+    assert sub_df.columns.isin(breast_cancer_df.columns).sum() == n_features+1
+    # check the number of splits
+    assert len(sub_df["partition"].value_counts()) == n_splits
+    # regression
+    sub_df = bc_regression.create_submittable_dataframe(breast_cancer_df,
                                                             target_name=target,
                                                             n_splits=6,
                                                             random_state=conf.random_state,
@@ -104,3 +116,14 @@ def test_creating_submittable_dataframe():
         pass
     else: 
         raise Exception(f"A submittable df was created with the target {absent_target}")
+
+    
+@pytest.mark.order(4)
+def test_submitting_datarobot_project():
+    """Tests submitting a datarobot project using rapa.
+
+        1. classification project
+    """
+
+    bc_classification = rapa.Project.Classification()
+
