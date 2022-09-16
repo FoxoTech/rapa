fd5485c3e2ee729654e70947b6673b26ddaa4fa4
viabard <joshuaschaaf@gmail.com>
2022-09-16 15:59:27 -0700

    ignored test file and changed conf
    changed conf to correct starred id and added names of models
    removed test ipython notebook from repo

--- tests/conf.py
+++ tests/conf.py
@@ -1,13 +1,13 @@
 # classification and regression project known 'bests' for testing
 classification_project_name = 'TUTORIAL_breast_cancer'
 classification_project_id = '62b5dc8249ed6b10669ab468'
-classification_starred_model_id = '62b5e1aeddc5c75c4d91cf7a'
-best_AUC_model_id = '62b5e1aeddc5c75c4d91cf84'
+classification_starred_model_id = '62b5e1aeddc5c75c4d91cf7a' # Logistic Regression
+best_AUC_model_id = '62b5e1aeddc5c75c4d91cf84' # Elastic-Net Classifier (L2 / Binomial Deviance)
 
 regression_project_name = 'GA_TESTS rapa==0.1.0_regression'
 regression_project_id = '62ffd722141fc37193f0c508'
-regression_starred_model_id = '62b5e1aeddc5c75c4d91cf7a'
-best_RMSE_model_id = '62ffd77ce69bf60110cee63c'
+regression_starred_model_id = '62ffd77ce69bf60110cee639' # Elastic-Net Regressor (mixing alpha=0.5 / Gamma Deviance)
+best_RMSE_model_id = '62ffd77ce69bf60110cee63c' # Light Gradient Boosting on ElasticNet Predictions (Gamma Loss)
 
 # prefix for testing
 featurelist_prefix = 'TEST_0.0.9'
